b997489438a84c4ece0134dc6a07c9d344ff2b67
unknown <s35385@pjwstk.edu.pl>
2026-04-28 21:55:56 +0200

    Dodanie abstrakcyjnej klasy bazowej

---
+++ src/MembershipPlan.java
@@ -0,0 +1,47 @@
+public abstract class MembershipPlan implements Billable {
+    private String planCode;
+    private String clientName;
+    private int months;
+    private double baseMonthlyFee;
+    private boolean autoRenew;
+
+    public MembershipPlan(String planCode, String clientName, int months, double baseMonthlyFee, boolean autoRenew) {
+        this.planCode = planCode;
+        this.clientName = clientName;
+        this.months = months;
+        this.baseMonthlyFee = baseMonthlyFee;
+        this.autoRenew = autoRenew;
+    }
+
+    public String getPlanCode() { return planCode; }
+    public String getClientName() { return clientName; }
+    public int getMonths() { return months; }
+    public double getBaseMonthlyFee() { return baseMonthlyFee; }
+    public boolean isAutoRenew() { return autoRenew; }
+
+    public abstract String getPlanType();
+    @Override
+    public abstract double calculateMonthlyNetPrice();
+
+    @Override
+    public double calculateMonthlyGrossPrice() {
+        return calculateMonthlyNetPrice() * 1.23;
+    }
+
+    public double calculateTotalNetPrice() {
+        return calculateMonthlyNetPrice() * months;
+    }
+
+    public final void printSummary() {
+        System.out.println("Plan: " + getPlanType() + " [" + planCode + "]");
+        System.out.println("Client: " + clientName);
+        System.out.println("Monthly Net: " + calculateMonthlyNetPrice());
+        System.out.println("Monthly Gross: " + calculateMonthlyGrossPrice());
+        System.out.println("Total Contract Net: " + calculateTotalNetPrice());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Code: %s, Client: %s, Duration: %d months, Auto-renew: %s",planCode, clientName, months, autoRenew);
+    }
+}
